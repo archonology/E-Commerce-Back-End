8858e944f01496016e18cc87333515726e6d440f
Your Name <you@example.com>
2022-10-03 12:52:07 -0500

    working on route errors for POST product

--- models/index.js
+++ models/index.js
@@ -11,14 +11,20 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   //this foreign key needs to refer to the child category, not the parent category (which would just be "id" - "category_id" is the name of the associated fields) -- maybe, but not sure
-  foreignKey: "id",
+  foreignKey: "category_id",
   onDelete: "CASCADE",
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, { through: ProductTag, 
+  foreignKey: "product_id",
+  onDelete: "CASCADE",
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, { through: ProductTag, 
+  foreignKey: "tag_id",
+  onDelete: "CASCADE",
+});
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -38,6 +38,16 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
+// router.post("/", async (req, res) => {
+//   // create a new category -- needs attention?
+//   try {
+//     const productData = await Product.create(req.body);
+//     res.status(200).json(productData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
